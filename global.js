fbfc7501da586a569cdfc6bc28f35e252c873c6e
clairelee-ucsd <165743578+clairelee-ucsd@users.noreply.github.com>
2025-01-22 23:03:35 -0800

    update global.js

--- global.js
+++ global.js
@@ -8,16 +8,18 @@ let pages = [
     { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
     { url: 'contact/', title: 'Contact'},
-    { url: 'resume/', title: 'resume'}
+    { url: 'resume/', title: 'Resume'},
+    { url: 'https://github.com/clairelee-ucsd', title: 'GitHub profile'}
   ];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
     url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
 
