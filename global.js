18903e31e768bd27e9796a7d2c3272ed717da4bd
clairelee-ucsd <165743578+clairelee-ucsd@users.noreply.github.com>
2025-01-22 22:58:44 -0800

    part of automatic navigation menu

--- global.js
+++ global.js
@@ -4,9 +4,29 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-navLinks = $$('nav a');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'resume/', title: 'resume'}
+  ];
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
-currentLink?.classList.add('current');
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+
+// navLinks = $$('nav a');
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+// currentLink?.classList.add('current');
