96cf4ba2ca7be3cc79cf946b253e1efcd10dbce2
clairelee-ucsd <165743578+clairelee-ucsd@users.noreply.github.com>
2025-01-23 01:05:11 -0800

    update global.js

--- global.js
+++ global.js
@@ -59,8 +59,14 @@ for (let p of pages) {
 
   const select = document.querySelector('.color-scheme select');
 
+  if ('colorScheme' in localStorage){
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+  }
+
   select.addEventListener('input', function (event) {
     console.log('color scheme changed to', event.target.value);
+    localStorage.colorScheme = event.target.value;
   });
 
   document.documentElement.style.setProperty('color-scheme', event.target.value);
