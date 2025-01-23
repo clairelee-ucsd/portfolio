7f35c1222cf754365b0e8b189aa088c8f85b2578
clairelee-ucsd <165743578+clairelee-ucsd@users.noreply.github.com>
2025-01-22 23:26:35 -0800

    update global.js

--- global.js
+++ global.js
@@ -4,8 +4,6 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const BASE__URL = 'https://clairelee-ucsd.github.io/portfolio/';
-
 let pages = [
     { url: '', title: 'Home' },
     { url: 'projects/', title: 'Projects' },
@@ -17,6 +15,7 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const BASE_URL = 'https://clairelee-ucsd.github.io/portfolio/';
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 for (let p of pages) {
@@ -24,8 +23,10 @@ for (let p of pages) {
     let title = p.title;
 
     if (!ARE_WE_HOME && !url.startsWith('http')) {
-        url = BASE__URL + '../' + url;
-      }
+        url = BASE_URL + url + 'index.html';
+    } else if (url && !url.startsWith('http')) {
+        url = BASE_URL + url + 'index.html';
+    }
 
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   }
