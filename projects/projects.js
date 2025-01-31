185e87f54f95f96da31a66d9c463a9c39a0b01fa
clairelee-ucsd <165743578+clairelee-ucsd@users.noreply.github.com>
2025-01-31 15:20:23 -0800

    update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,16 +2,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
+const projectCount = projects.length;
+const projectsTitle = document.querySelector('.projects-title');
+projectsTitle.textContent = `${projectCount} Projects`;
 renderProjects(projects, projectsContainer, 'h2');
-
-async function updateProjectCount() {
-    const response = await fetch('../lib/projects.json');
-    const projects = await response.json(); 
-
-    const projectsTitle = document.querySelector('.projects-title');
-    const projectCount = projects.length;
-
-    projectsTitle.textContent = `${projectCount} Projects`;
-}
-
-updateProjectCount();
