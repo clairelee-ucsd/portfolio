9f0160062fca8d3b23e9ae8366e374135acd9970
clairelee-ucsd <165743578+clairelee-ucsd@users.noreply.github.com>
2025-02-07 03:13:27 -0800

    add legend to pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -3,8 +3,15 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2, 3, 4, 5, 5];
-let sliceGenerator = d3.pie();
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+    ];
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
@@ -16,6 +23,14 @@ arcs.forEach((arc, idx) => {
       .attr('fill', colors(idx))
       .attr('transform', 'translate(0, 0)');
   });
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in
+    .attr('class', 'legend-item')
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the i
+    })
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
