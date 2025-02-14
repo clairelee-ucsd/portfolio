3939c3369a6aca688597f3de075a2065fce33192
clairelee-ucsd <165743578+clairelee-ucsd@users.noreply.github.com>
2025-02-14 03:37:51 -0800

    add row conversion function

--- meta/main.js
+++ meta/main.js
@@ -8,3 +8,14 @@ async function loadData() {
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
+
+async function loadData() {
+    data = await d3.csv('loc.csv', (row) => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + 'T00:00' + row.timezone),
+      datetime: new Date(row.datetime),
+    }));
+  }
